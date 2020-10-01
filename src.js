dfddad15c7a7369f5bf5695d99f9096d962cdd4d
bananajr <jake@jakesprouse.net>
2020-10-01 09:22:13 -0700

    Added support for Firefox

--- src.js
+++ src.js
@@ -13,8 +13,20 @@ function run(argv) {
         var tab = app.windows[0].currentTab;
         return { url: tab.url(), title: tab.name() };
       }
+    },
+    { name: "firefox",
+      fn: function(app) {
+	    var win = app.windows[0];
+        var url = getURLFromFirefox(app);
+        if (url) {
+	        return { url: url, title: win.name() };
+        }
+        return { }
+	  }
     }
   ];
+
+  var doCustomize = (argv[0] !== '0');
 
   const frontAppName = Application('System Events').applicationProcesses.where({ frontmost: true }).name()[0]
 
@@ -30,7 +42,9 @@ function run(argv) {
     console.log(p.title);
     console.log(p.url);
     if (p.hasOwnProperty('title') && p.hasOwnProperty('url')) {
-      p = customize(p);
+      if (doCustomize) {
+      	p = customize(p);
+      }
       copyToClipboard(p);
       return p.title + ' - ' + p.url;
     }
@@ -40,10 +54,7 @@ function run(argv) {
 
 
 function customize(p) {
-  var sourceIndex = p.title.lastIndexOf(' - ');
-  if (sourceIndex < 0) {
-     sourceIndex = p.title.lastIndexOf(' | ');
-  }
+  var sourceIndex = p.title.lastIndexOf(' | ');
   if (sourceIndex >= 0) {
     p.source = p.title.substr(sourceIndex + 3);
     p.title = p.title.substr(0, sourceIndex);
@@ -62,21 +73,44 @@ function customize(p) {
 
 
 function copyToClipboard(p) {
-  var htmlStr = '<a href="' + p.url + '">' + p.title +'</a>';
-  var mdStr   = '[' + p.title + '](' + p.url + ')';
-  var rtfStr  = '{\\rtf1\\ansi\deff0{\\field{\\*\\fldinst{HYPERLINK "' + p.url +
-                '"}}{\\fldrslt ' + p.title + '}}}';
-
+  var sourceStr = '';
   if (p.hasOwnProperty('source')) {
 	var sourceStr = ' (' + p.source + ')';
-    htmlStr += sourceStr;
-    mdStr   += sourceStr;
-    rtfStr  += sourceStr;
   }
+
+  var htmlStr = '<a href="' + p.url + '">' + p.title +'</a>' + sourceStr;
+  var mdStr   = '[' + p.title + '](' + p.url + ')' + sourceStr;
+  var rtfStr  = '{\\rtf1\\ansi\deff0{\\field{\\*\\fldinst{HYPERLINK "' + p.url +
+                '"}}{\\fldrslt ' + p.title + '}}' + sourceStr + '}';
 
   var pb = $.NSPasteboard.generalPasteboard;
   pb.clearContents;
   pb.setStringForType($(mdStr),   $.NSPasteboardTypeString);
   pb.setStringForType($(htmlStr), $.NSPasteboardTypeHTML);
   pb.setStringForType($(rtfStr) , $.NSPasteboardTypeRTF);
+}
+
+
+function getURLFromFirefox(app) {
+  var pb = $.NSPasteboard.generalPasteboard;
+  var pbcount = pb.changeCount;
+  
+  var se = Application('System Events');
+  se.keystroke('l', { using: 'command down' });
+  delay(0.1);
+  se.keystroke('c', { using: 'command down' });
+  delay(0.1);
+  se.keyCode(53); // escape
+
+  for (var count = 0; count < 20; ++count) {
+	if (pb.changeCount != pbcount) {
+      console.log("pasteboard changed");
+      var url = ObjC.unwrap(pb.stringForType($.NSPasteboardTypeString));
+	  console.log(url);
+      return url;
+    }
+    delay(0.1)
+  }
+  console.log("pasteboard never changed");
+  return null;
 }
